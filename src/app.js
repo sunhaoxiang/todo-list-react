da45f5fb8c9e72b5cb60f63a5c2966d7cbbb155f
孙昊翔 <673686754@qq.com>
2018-01-14 22:10:47 +0800

    no message

--- src/app.js
+++ src/app.js
@@ -6,21 +6,67 @@ import './common/style/index.css'
 export default class App extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      todosData: []
+    }
+
+    this.handleKeyDownPost = this.handleKeyDownPost.bind(this)
+    this.onDestroy = this.onDestroy.bind(this)
+    this.onClearCompleted.bind(this)
+  }
+
+  handleKeyDownPost (ev) {
+    if (ev.keyCode !== 13) return
+    let value = ev.target.value.trim();
+    if (value === '') return
+    let todo = {}
+    todo.id = new Date().getTime()
+    todo.value = value
+    todo.hasCompleted = false
+    let {todosData} = this.state
+    todosData.push(todo)
+    this.setState({todosData})
+    ev.target.value = ''
+  }
+
+  onDestroy (todo) {
+    let {todosData} = this.state
+    todosData = todosData.filter(e => {
+      return e.id !== todo.id
+    })
+
+    this.setState({todosData})
+  }
+
+  onClearCompleted () {
+    let {todosData} = this.state
+    todosData = todosData.filter(e => {
+      return !elt.hasCompleted
+    })
+
+    this.setState({todosData})
   }
 
   render () {
+    let {handleKeyDownPost, onDestroy, onClearCompleted} = this
+    let {todosData} = this.state
+    let items = null
+    items = todosData.map((e, i) => {
+      return (
+        <Item {...{onDestroy, todo:e}} key={i}/>
+      )
+    })
+
     return (
       <div>
-        <header calssName="header">
+        <header className="header">
           <h1>todos</h1>
-          <input type="text" className="new-todo"/>
+          <input type="text" className="new-todo" onKeyDown={handleKeyDownPost}/>
         </header>
         <section className="main">
           <input type="checkbox" className="toggle-all"/>
           <ul className="todo-list">
-            <Item/>
-            <Item/>
-            <Item/>
+            {items}
           </ul>
         </section>
         <Footer/>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,27 @@
+let propTypes = {
+  todo: PT.object,
+  onDestroy: PT.func
+}
+
 export default class Item extends React.Component {
   constructor (props) {
     super(props)
   }
 
   render () {
+    let {todo, onDestroy} = this.props
+
     return (
       <li>
         <div className="view">
           <input type="checkbox" className="toggle"/>
-          <label>content</label>
-          <button className="destory"></button>
+          <label>{todo.value}</label>
+          <button className="destroy" onClick={(ev) => onDestroy(todo)}></button>
         </div>
         <input type="text" className="edit"/>
       </li>
     )
   }
 }
+
+Item.propTypes = propTypes
